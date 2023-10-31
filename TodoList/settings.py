d961ac763da3bc1a446f81d5c48518ebbc7908bc
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-10-31 14:50:30 +0200

    change class Meta from Task model

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "bootstrap4",
     "crispy_forms",
+    "crispy_bootstrap4",
     "debug_toolbar",
     "notebook",
 
--- notebook/models.py
+++ notebook/models.py
@@ -16,7 +16,7 @@ class Task(models.Model):
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
     class Meta:
-        ordering = ("created", "is_done")
+        ordering = ("is_done", "created")
 
     def __str__(self):
         return self.content
